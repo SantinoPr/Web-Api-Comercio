a81f6479a210106dd64b06ddf28db40de42cea16
SantinoPr <santino-albarello@hotmail.com>
2026-01-16 12:43:07 -0300

    agregado de dokc file y version de .net 10 a .net 8 y eliminacion de openApi en program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddScoped<ProductoData>();
 builder.Services.AddScoped<VentaData>();
 builder.Services.AddScoped<EnvioData>();
@@ -18,7 +17,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
